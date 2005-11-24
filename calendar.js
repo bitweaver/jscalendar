80c99d8186be11a0439b69a61767f7e3855e2d13
Max Kremmel <xing@synapse.plus.com>
2005-11-24 21:08:39 +0000

    clean up calendar display, make XHTML compliant, fix jscalendar and use custom bitweaver css file - much more solid than supplied css files

--- calendar.js
+++ calendar.js
@@ -12,7 +12,7 @@
  * Read the entire license text here: http://www.gnu.org/licenses/lgpl.html
  */
 
-// $Id: calendar.js,v 1.1 2005/10/03 08:11:48 squareing Exp $
+// $Id: calendar.js,v 1.1.1.1.2.1 2005/11/24 21:08:39 squareing Exp $
 
 /** The Calendar object constructor. */
 Calendar = function (firstDayOfWeek, dateStr, onSelected, onClose) {
@@ -739,7 +739,8 @@ Calendar.prototype.create = function (_par) {
 
 	var div = Calendar.createElement("div");
 	this.element = div;
-	div.className = "calendar";
+	// renamed class from calendar to jscalendar - xing
+	div.className = "jscalendar";
 	if (this.isPopup) {
 		div.style.position = "absolute";
 		div.style.display = "none";
