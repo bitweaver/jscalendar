ac2e2fcdb452cd244feffa4c9b4d4d1620428105
Sandro Santilli <strk@keybit.net>
2010-10-10 09:50:59 +0200

    Add missing Calendar._FD variable in italian translation. W/out this the calendar breaks

--- lang/calendar-it.js
+++ lang/calendar-it.js
@@ -59,6 +59,10 @@ Calendar._MN = new Array
  "Novembre",
  "Dicembre");
 
+// First day of the week. "0" means display Sunday first, "1" means display
+// Monday first, etc.
+Calendar._FD = 1;
+
 // short month names
 Calendar._SMN = new Array
 ("Gen",
